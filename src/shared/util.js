mozilla/pdf.js

commit 3888a993b125e22fdfffd36bf603baf830e6fbe2

Remove the `URL` checks in the `createObjectURL` utility function, since the `URL` polyfill have made them redundant
Also, this changes `createBlob` to throw when `Blob` isn't supported.

src/shared/util.js
@@ -1190,7 +1190,7 @@ var createBlob = function createBlob(data, contentType) {
   if (typeof Blob !== 'undefined') {
     return new Blob([data], { type: contentType });
   }
-  warn('The "Blob" constructor is not supported.');
+  throw new Error('The "Blob" constructor is not supported.');
 };
 
 var createObjectURL = (function createObjectURLClosure() {
@@ -1198,9 +1198,8 @@ var createObjectURL = (function createObjectURLClosure() {
   var digits =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
-  return function createObjectURL(data, contentType, forceDataSchema) {
-    if (!forceDataSchema &&
-        typeof URL !== 'undefined' && URL.createObjectURL) {
+  return function createObjectURL(data, contentType, forceDataSchema = false) {
+    if (!forceDataSchema) {
       var blob = createBlob(data, contentType);
       return URL.createObjectURL(blob);
     }

0x01050/customized-pdfjs-for-abdullah

commit 23e41e63269896f42b3bd643b9a75ace7cc6414f

Restore the `URL.createObjectURL` check to the `createObjectURL` utility function (issue 8344)
This is a regression from commit https://github.com/mozilla/pdf.js/commit/3888a993b125e22fdfffd36bf603baf830e6fbe2.

It turns out the even though we have a `URL` polyfill, it's still dependent on the existence of native `URL.{createObjectURL, revokeObjectURL}` functions.
Since no such thing exists in Node.js, our `createObjectURL` utility function breaks there.

src/shared/util.js
@@ -1199,7 +1199,7 @@ var createObjectURL = (function createObjectURLClosure() {
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
   return function createObjectURL(data, contentType, forceDataSchema = false) {
-    if (!forceDataSchema) {
+    if (!forceDataSchema && URL.createObjectURL) {
       var blob = createBlob(data, contentType);
       return URL.createObjectURL(blob);
     }